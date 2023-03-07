8e1cceaaa60b33296bfe1fc116e41fc6f86dd9cd
dspray95 <dspray95@gmail.com>
2023-03-07 17:59:30 +0000

    fix: added load and run to tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,20 +1,20 @@
-mod registers;
 mod memory;
+mod registers;
 
-use registers::Register;
 use memory::Memory;
+use registers::Register;
 
 pub struct Cpu {
     register: Register,
-    memory: Memory
+    memory: Memory,
 }
 
 impl Cpu {
-    pub fn new() -> Cpu{
-        return Cpu{
+    pub fn new() -> Cpu {
+        return Cpu {
             register: Register::new(),
-            memory: Memory::new()
-        }
+            memory: Memory::new(),
+        };
     }
 
     pub fn load_and_run(&mut self, program: Vec<u8>) {
@@ -34,7 +34,7 @@ impl Cpu {
             match opscode {
                 0x00 => {
                     return;
-                },
+                }
                 0xA9 => {
                     let param = self.memory.read(self.register.program_counter);
                     self.register.program_counter += 1;
@@ -42,27 +42,28 @@ impl Cpu {
                 }
                 0xAA => self.tax(),
                 0xE8 => self.inx(),
-                _ => todo!()
+                _ => todo!(),
             }
         }
-
-        
     }
 
-    pub fn tax(&mut self){
+    pub fn tax(&mut self) {
         //0xAA
         self.register.x = self.register.accumulator;
-        self.register.update_zero_and_negative_flags(self.register.x);
+        self.register
+            .update_zero_and_negative_flags(self.register.x);
     }
 
-    pub fn lda(&mut self, value: u8){
+    pub fn lda(&mut self, value: u8) {
         self.register.accumulator = value;
-        self.register.update_zero_and_negative_flags(self.register.accumulator);
+        self.register
+            .update_zero_and_negative_flags(self.register.accumulator);
     }
 
-    pub fn inx(&mut self){
+    pub fn inx(&mut self) {
         self.register.x += 1;
-        self.register.update_zero_and_negative_flags(self.register.x);
+        self.register
+            .update_zero_and_negative_flags(self.register.x);
     }
 }
 
@@ -73,10 +74,10 @@ mod test {
     #[test]
     fn test_0xa9_lda_iimediate_load_data() {
         let mut cpu = Cpu::new();
-        //                | 2b instr || exit | 
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        //| 2b instr || exit |
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         //should load value from next cell into register
-        assert_eq!(cpu.register.accumulator, 0x05); 
+        assert_eq!(cpu.register.accumulator, 0x05);
         //zflag is set
         assert!(cpu.register.processor_status & 0b0000_0010 == 0b00);
         //negative flag is set
@@ -86,49 +87,49 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = Cpu::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         //This makes sure the zflag is set if the valiue in the accumulator is 0
         //is 0
         assert!(cpu.register.processor_status & 0b0000_0010 == 0b10);
     }
 
     #[test]
-    fn test_0xa9_lda_negative_flag(){
+    fn test_0xa9_lda_negative_flag() {
         let mut cpu = Cpu::new();
-        cpu.interpret(vec![0xa9, 0b0000_0011, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0b0000_0011, 0x00]);
         assert!(cpu.register.processor_status & 0b1000_0000 == 0b10);
     }
 
     #[test]
-    fn test_0xaa_tax(){
+    fn test_0xaa_tax() {
         let mut cpu = Cpu::new();
         cpu.register.accumulator = 9;
-        cpu.interpret(vec![0xAA, 0x00]);
+        cpu.load_and_run(vec![0xAA, 0x00]);
         assert!(cpu.register.x == 9);
     }
 
     #[test]
-    fn test_0xe8_inx(){
+    fn test_0xe8_inx() {
         let mut cpu = Cpu::new();
         cpu.register.x = 1;
-        cpu.interpret(vec![0xE8, 0x00]);
+        cpu.load_and_run(vec![0xE8, 0x00]);
         assert_eq!(cpu.register.x, 2);
     }
 
     #[test]
     fn test_5_ops_working_together() {
         let mut cpu = Cpu::new();
-        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
-  
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
         assert_eq!(cpu.register.x, 0xc1)
     }
- 
-     #[test]
-     fn test_inx_overflow() {
-         let mut cpu = Cpu::new();
-         cpu.register.x = 0xff;
-         cpu.interpret(vec![0xe8, 0xe8, 0x00]);
- 
-         assert_eq!(cpu.register.x, 1)
-     }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = Cpu::new();
+        cpu.register.x = 0xff;
+        cpu.load_and_run(vec![0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register.x, 1)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod cpu;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Loading and running program...");
     let mut processor = cpu::Cpu::new();
     let program: Vec<u8> = vec![0, 0, 0, 0];
-    processor.interpret(program);
+    processor.load_and_run(program);
+    println!("...finished!")
 }
