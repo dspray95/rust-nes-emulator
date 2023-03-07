e5fc513e021a480622d97444251e17e97d1a6dbd
dspray95 <dspray95@gmail.com>
2023-03-07 19:30:53 +0000

    (chapter 3.3) lda and sta opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,8 +1,23 @@
 mod memory;
+mod opcodes;
 mod registers;
 
 use memory::Memory;
 use registers::Register;
+
+#[derive(Debug)]
+pub enum AddressingMode {
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    IndirectX,
+    IndirectY,
+    NoneAddressing,
+}
 
 pub struct Cpu {
     register: Register,
@@ -19,30 +34,97 @@ impl Cpu {
 
     pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
-        self.run()
+        self.reset();
+        self.run();
     }
 
     pub fn load(&mut self, program: Vec<u8>) {
         self.memory.load_program(program);
-        self.register.program_counter = 0x8000;
+        self.memory.write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn reset(&mut self) {
+        self.register.accumulator = 0;
+        self.register.x = 0;
+        self.register.y = 0;
+        self.register.processor_status = 0;
+
+        self.register.program_counter = self.memory.read_u16(0xFFFC);
     }
 
     pub fn run(&mut self) {
+        let ref opcodes = *opcodes::OPCODES_MAP;
         loop {
-            let opscode = self.memory.read(self.register.program_counter);
+            let code = self.memory.read(self.register.program_counter);
             self.register.program_counter += 1;
-            match opscode {
+            let program_counter_state = self.register.program_counter;
+
+            let opcode = opcodes
+                .get(&code)
+                .expect(&format!("OpCode {:x} is not recognized", code));
+
+            match code {
+                0xA9 | 0xA5 | 0xB5 | 0xAD | 0xBD | 0xB9 | 0xA1 | 0xB1 => {
+                    self.lda(&opcode.mode);
+                }
+                0x85 | 0x96 | 0x8D | 0x9D | 0x99 | 0x81 | 0x91 => self.sta(&opcode.mode),
+                0xAA => self.tax(),
+                0xE8 => self.inx(),
                 0x00 => {
                     return;
                 }
-                0xA9 => {
-                    let param = self.memory.read(self.register.program_counter);
-                    self.register.program_counter += 1;
-                    self.lda(param);
-                }
-                0xAA => self.tax(),
-                0xE8 => self.inx(),
                 _ => todo!(),
+            }
+
+            if program_counter_state == self.register.program_counter {
+                self.register.program_counter += (opcode.len - 1) as u16;
+            }
+        }
+    }
+
+    pub fn get_operand_anddress(&self, mode: &AddressingMode) -> u16 {
+        match mode {
+            AddressingMode::Immediate => self.register.program_counter,
+            AddressingMode::ZeroPage => self.memory.read(self.register.program_counter) as u16,
+            AddressingMode::ZeroPageX => {
+                let pos = self.memory.read(self.register.program_counter);
+                let addr = pos.wrapping_add(self.register.x) as u16;
+                return addr;
+            }
+            AddressingMode::ZeroPageY => {
+                let pos = self.memory.read(self.register.program_counter);
+                let addr = pos.wrapping_add(self.register.y) as u16;
+                return addr;
+            }
+            AddressingMode::Absolute => self.memory.read_u16(self.register.program_counter),
+            AddressingMode::AbsoluteX => {
+                let base = self.memory.read_u16(self.register.program_counter);
+                let addr = base.wrapping_add(self.register.x as u16);
+                return addr;
+            }
+            AddressingMode::AbsoluteY => {
+                let base = self.memory.read_u16(self.register.program_counter);
+                let addr = base.wrapping_add(self.register.y as u16);
+                return addr;
+            }
+            AddressingMode::IndirectX => {
+                let base = self.memory.read(self.register.program_counter);
+
+                let ptr: u8 = (base as u8).wrapping_add(self.register.x);
+                let lo = self.memory.read(ptr as u16);
+                let hi = self.memory.read(ptr.wrapping_add(1) as u16);
+                (hi as u16) << 8 | (lo as u16)
+            }
+            AddressingMode::IndirectY => {
+                let base = self.memory.read(self.register.program_counter);
+
+                let ptr: u8 = (base as u8).wrapping_add(self.register.y);
+                let lo = self.memory.read(ptr as u16);
+                let hi = self.memory.read(ptr.wrapping_add(1) as u16);
+                (hi as u16) << 8 | (lo as u16)
+            }
+            AddressingMode::NoneAddressing => {
+                panic!("mode {:?} is not supported", mode);
             }
         }
     }
@@ -54,16 +136,24 @@ impl Cpu {
             .update_zero_and_negative_flags(self.register.x);
     }
 
-    pub fn lda(&mut self, value: u8) {
+    pub fn lda(&mut self, mode: &AddressingMode) {
+        let address = self.get_operand_anddress(mode);
+        let value = self.memory.read(address);
+
         self.register.accumulator = value;
         self.register
             .update_zero_and_negative_flags(self.register.accumulator);
     }
 
     pub fn inx(&mut self) {
-        self.register.x += 1;
+        self.register.x = self.register.x.wrapping_add(1);
         self.register
             .update_zero_and_negative_flags(self.register.x);
+    }
+
+    pub fn sta(&mut self, mode: &AddressingMode) {
+        let address = self.get_operand_anddress(mode);
+        self.memory.write(address, self.register.accumulator);
     }
 }
 
@@ -72,14 +162,14 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_0xa9_lda_iimediate_load_data() {
+    fn test_0xa9_lda_immediate_load_data() {
         let mut cpu = Cpu::new();
         //| 2b instr || exit |
         cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         //should load value from next cell into register
-        assert_eq!(cpu.register.accumulator, 0x05);
+        assert_eq!(cpu.register.accumulator, 5);
         //zflag is set
-        assert!(cpu.register.processor_status & 0b0000_0010 == 0b00);
+        assert!(cpu.register.processor_status & 0b0000_0010 == 0);
         //negative flag is set
         assert!(cpu.register.processor_status & 0b1000_0000 == 0);
     }
@@ -96,24 +186,16 @@ mod test {
     #[test]
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = Cpu::new();
-        cpu.load_and_run(vec![0xa9, 0b0000_0011, 0x00]);
-        assert!(cpu.register.processor_status & 0b1000_0000 == 0b10);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.register.processor_status & 0b0000_0010 == 0b10);
     }
 
     #[test]
-    fn test_0xaa_tax() {
+    fn test_0xaa_tax_move_a_to_x() {
         let mut cpu = Cpu::new();
-        cpu.register.accumulator = 9;
-        cpu.load_and_run(vec![0xAA, 0x00]);
-        assert!(cpu.register.x == 9);
-    }
+        cpu.load_and_run(vec![0xa9, 0x0A, 0xaa, 0x00]);
 
-    #[test]
-    fn test_0xe8_inx() {
-        let mut cpu = Cpu::new();
-        cpu.register.x = 1;
-        cpu.load_and_run(vec![0xE8, 0x00]);
-        assert_eq!(cpu.register.x, 2);
+        assert_eq!(cpu.register.x, 10)
     }
 
     #[test]
@@ -127,9 +209,18 @@ mod test {
     #[test]
     fn test_inx_overflow() {
         let mut cpu = Cpu::new();
-        cpu.register.x = 0xff;
-        cpu.load_and_run(vec![0xe8, 0xe8, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xff, 0xaa, 0xe8, 0xe8, 0x00]);
 
         assert_eq!(cpu.register.x, 1)
+    }
+
+    #[test]
+    fn test_lda_from_memory() {
+        let mut cpu = Cpu::new();
+        cpu.memory.write(0x10, 0x55);
+
+        cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
+
+        assert_eq!(cpu.register.accumulator, 0x55);
     }
 }
--- src/cpu/memory.rs
+++ src/cpu/memory.rs
@@ -1,33 +1,33 @@
-pub struct Memory{
-    memory: [u8; 0xFFFF]
+pub struct Memory {
+    memory: [u8; 0xFFFF],
 }
 
 impl Memory {
-    pub fn new() -> Memory{
-        return Memory{
-            memory: [0; 0xFFFF]
-        }
+    pub fn new() -> Memory {
+        return Memory {
+            memory: [0; 0xFFFF],
+        };
     }
 
-    pub fn load_program(&mut self, program: Vec<u8>){
-        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+    pub fn load_program(&mut self, program: Vec<u8>) {
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
     }
 
-    pub fn read(&self, address: u16) -> u8{
+    pub fn read(&self, address: u16) -> u8 {
         self.memory[address as usize]
     }
 
-    pub fn write(&mut self, address: u16, data: u8){
+    pub fn write(&mut self, address: u16, data: u8) {
         self.memory[address as usize] = data;
     }
 
-    fn read_u16(&mut self, pos: u16) -> u16 {
+    pub fn read_u16(&self, pos: u16) -> u16 {
         let lo = self.read(pos) as u16;
         let hi = self.read(pos + 1) as u16;
-        return (hi << 8) | (lo as u16)
+        return (hi << 8) | (lo as u16);
     }
 
-    fn write_u16(&mut self, pos: u16, data: u16) {
+    pub fn write_u16(&mut self, pos: u16, data: u16) {
         let hi = (data >> 8) as u8;
         let lo = (data & 0xff) as u8;
         self.write(pos, lo);
